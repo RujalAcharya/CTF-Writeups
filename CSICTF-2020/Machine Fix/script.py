880748a1cfac46ff687a40c8f409703c1a15d223
Rujal Acharya <59135589+TheFakeS1imShady@users.noreply.github.com>
2020-07-22 13:13:18 +0545

    Update script.py

--- CSICTF-2020/Machine Fix/script.py
+++ CSICTF-2020/Machine Fix/script.py
@@ -1,19 +1,2 @@
-req =  523693181734689806809285195318
-
-
-sol = 0
-i = 0
-j = 0
-
-for n in range(req):
-    sol +=1
-    i += 1
-    if i == 3:
-        i = 0
-        j +=1
-        sol +=1
-        if j == 3:
-            j = 0
-            sol += 1
-
-print(sol)
+output = lambda n: output(n//3) + n if n > 0 else 0
+print(output(523693181734689806809285195318))
